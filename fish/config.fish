46d1b296b9ac3d300d775607e5ec2389caf7b58f
yahikii <yasminhinel@gmail.com>
2026-04-14 23:53:00 +0200

    feat: add nvim alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    end
+	fish_add_path $HOME/.local/bin
+	alias docker 'podman'
+	alias vim="nvim"
+    	alias vi="nvim"
+end
 
 # Set up starship
 set -x STARSHIP_CONFIG ~/.config/starship.toml
@@ -8,3 +11,6 @@ starship init fish | source
 
 # Set up direnv
 direnv hook fish | source
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
